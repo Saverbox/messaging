c73a5021fa721e7464da9083c6a8b73830f9d936
Saverbox <schpat@icloud.com>
2023-05-24 16:20:35 +0200

    Modify: Change to Panache

--- src/main/java/ch/hftm/Blog.java
+++ src/main/java/ch/hftm/Blog.java
@@ -1,7 +1,13 @@
 package ch.hftm;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Blog {
-    
+    @Id @GeneratedValue
+    private Long id;
     private String title;
     private String content;
     
